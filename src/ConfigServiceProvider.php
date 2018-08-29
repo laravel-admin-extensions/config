a6b7c42dbe53f56cedfe3f0db9b880e0c9a0c6e6
Artem <artem.tverdokhlebov@icloud.com>
2018-08-29 14:39:48 +0300

    Test loading language file

--- src/ConfigServiceProvider.php
+++ src/ConfigServiceProvider.php
@@ -13,6 +13,8 @@ class ConfigServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+            $this->publishes([__DIR__.'/../resources/lang' => resource_path('lang')], 'laravel-admin-ext-config-lang');
         }
 
         Config::boot();
